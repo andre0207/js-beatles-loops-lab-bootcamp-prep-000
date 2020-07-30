2302f6cc133d1ffec46e65490e002f6ed185d308
Andre Pierre <andre.laurence94@gmail.com>
2020-07-30 13:34:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// add solution here
+let theBeatlesPlay = (musicians, instruments) => {
+ let newArr = [];
+  for (let i = 0; i < musicians.length; i++) {
+    newArr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArr
+}
+
+let johnLennonFacts = (array) => {
+  let newArr = [];
+  let i = 0
+  
+  while(i < array.length){
+    newArr.push(`${array[i]}!!!`)
+    i++
+  }
+  return newArr
+}
+
+let iLoveTheBeatles = (num) => {
+  let newArr = [];
+  
+  let i = 0
+  
+  do { newArr.push('I love the Beatles!') 
+  num++
+    
+  } while(i <= 8)
+  
+  return newArr
+}  
